79b5d52a7ef5e005991105dbadff27336e172605
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2022-11-19 20:36:00 +0530

    Added frontend to the set of allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "https://zocket-assignment.netlify.app",
+];
 
 module.exports = allowedOrigins;
