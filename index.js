a5a02d6fbe17b593beeb82690e27da38cc1690d7
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2022-11-19 19:56:00 +0530

    Fixed heroku error in index.js file

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
